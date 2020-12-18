cf1d687ae8c9fb24b9b7520ea0babd3f5e0b6fde
Lev Nachmanson <levnach@hotmail.com>
2020-12-18 14:10:27 -0800

    make vertex() a method

--- src/point.spec.ts
+++ src/point.spec.ts
@@ -1,17 +1,18 @@
-import { Point } from "./utils/geometry/point";
-import {VertexId, vertex, intersect, parallelogramByCornerSideSide, parallelogramOfTwo} from "./utils/geometry/parallelogram";
+// eslint-disable-next-line import/no-unresolved
+import { Point } from './utils/geometry/point';
+import { VertexId, intersect, parallelogramByCornerSideSide, parallelogramOfTwo } from "./utils/geometry/parallelogram";
 
 test("point test", () => {
-    const a : number = 1; 
-    const b : number = 2; 
-    const c: number = 3.1; 
+    const a: number = 1;
+    const b: number = 2;
+    const c: number = 3.1;
     const d: number = 5.9;
 
     const p1: Point = new Point(a, b);
     const p2: Point = new Point(c, d);
 
     console.log(p1.length());
-    expect(p1.length()).toBe(Math.sqrt(a*a+b*b))
+    expect(p1.length()).toBe(Math.sqrt(a * a + b * b))
 
     var resultPoint = p1.add(p2);
     expect(resultPoint.x).toBe(a + c);
@@ -22,49 +23,49 @@ test("point test", () => {
     expect(resultPoint.y).toBe(b - d);
 
     resultPoint = p1.mult(2);
-    expect(resultPoint.x).toBe(a*2);
-    expect(resultPoint.y).toBe(b*2); 
+    expect(resultPoint.x).toBe(a * 2);
+    expect(resultPoint.y).toBe(b * 2);
 });
 
-    test("parallelogram intersect test", () => {
+test("parallelogram intersect test", () => {
 
-        let pr0 =  parallelogramByCornerSideSide(new Point(0, 0),new Point(1, 0), new Point(0, 1) );
-        expect(pr0.corner.equal(new Point(0,0))).toBe(true); 
-        expect(vertex(pr0, 0).equal(pr0.corner)).toBe(true); 
-        
-        let pr1 =  parallelogramByCornerSideSide(new Point(2, 0),new Point(1, 0), new Point(0, 1) );
-        expect(intersect(pr0, pr1)).toBe(false);
-        let pr2 =  parallelogramByCornerSideSide(new Point(0, 0),new Point(2, 0), new Point(0, 1) );
-        expect(intersect(pr0, pr2)).toBe(true); 
-        let pr3 =  parallelogramByCornerSideSide(new Point(0, 0),new Point(2.0-0.00001, 0), new Point(0, 1) );
-        expect(intersect(pr1, pr3)).toBe(false);    
-    });
+    let pr0 = parallelogramByCornerSideSide(new Point(0, 0), new Point(1, 0), new Point(0, 1));
+    expect(pr0.corner.equal(new Point(0, 0))).toBe(true);
+    expect(pr0.vertex(0).equal(pr0.corner)).toBe(true);
+
+    let pr1 = parallelogramByCornerSideSide(new Point(2, 0), new Point(1, 0), new Point(0, 1));
+    expect(intersect(pr0, pr1)).toBe(false);
+    let pr2 = parallelogramByCornerSideSide(new Point(0, 0), new Point(2, 0), new Point(0, 1));
+    expect(intersect(pr0, pr2)).toBe(true);
+    let pr3 = parallelogramByCornerSideSide(new Point(0, 0), new Point(2.0 - 0.00001, 0), new Point(0, 1));
+    expect(intersect(pr1, pr3)).toBe(false);
+});
 
 test("parallelogram contains test", () => {
-    let par =  parallelogramByCornerSideSide(new Point(0, 0),new Point(1, 0), new Point(0, 1) );
-    let pOut =  vertex(par, VertexId.otherCorner).mult(1.1);
+    let par = parallelogramByCornerSideSide(new Point(0, 0), new Point(1, 0), new Point(0, 1));
+    let pOut = par.vertex(VertexId.otherCorner).mult(1.1);
     expect(par.contains(pOut)).toBe(false);
 
-    let par0 =  parallelogramByCornerSideSide(new Point(1, 0),new Point(2, 1), new Point(0, 1) );
-    let pIn =  vertex(par0, VertexId.otherCorner).add(vertex(par0, VertexId.Corner)).div(2);
+    let par0 = parallelogramByCornerSideSide(new Point(1, 0), new Point(2, 1), new Point(0, 1));
+    let pIn = par0.vertex(VertexId.otherCorner).add(par0.vertex(VertexId.Corner)).div(2);
     expect(par0.contains(pIn)).toBe(true);
-    
+
     let parTwo = parallelogramOfTwo(par, par0);
-    for (var i of [0,1,2,3]) {
-        expect(parTwo.contains(vertex(par, i))).toBe(true);
-        expect(parTwo.contains(vertex(par0, i))).toBe(true);
-    }    
+    for (var i of [0, 1, 2, 3]) {
+        expect(parTwo.contains(par.vertex(i))).toBe(true);
+        expect(parTwo.contains(par0.vertex(i))).toBe(true);
+    }
 });
 test("parallelogram seg case", () => {
-    let par =  parallelogramByCornerSideSide(new Point(0, 0),new Point(1, 0), new Point(1, Point.distanceEpsilon) );
-    let par0 =  parallelogramByCornerSideSide(new Point(0.5, 0),new Point(2, 1), new Point(2, 1) );
-    let par1 =  parallelogramByCornerSideSide(new Point(0.5, 0.1),new Point(2, 1), new Point(2, 1) );
-    let par2 =  parallelogramByCornerSideSide(new Point(0.5, -0.1),new Point(2, 1), new Point(2, 1) );
-    let par3 =  parallelogramByCornerSideSide(new Point(0.5, -0.1 - Point.distanceEpsilon/2),new Point(2, 1), new Point(2, 1) );
+    let par = parallelogramByCornerSideSide(new Point(0, 0), new Point(1, 0), new Point(1, Point.distanceEpsilon));
+    let par0 = parallelogramByCornerSideSide(new Point(0.5, 0), new Point(2, 1), new Point(2, 1));
+    let par1 = parallelogramByCornerSideSide(new Point(0.5, 0.1), new Point(2, 1), new Point(2, 1));
+    let par2 = parallelogramByCornerSideSide(new Point(0.5, -0.1), new Point(2, 1), new Point(2, 1));
+    let par3 = parallelogramByCornerSideSide(new Point(0.5, -0.1 - Point.distanceEpsilon / 2), new Point(2, 1), new Point(2, 1));
     expect(intersect(par, par0)).toBe(true);
-    expect(intersect(par, par1)).toBe(false);    
+    expect(intersect(par, par1)).toBe(false);
     expect(intersect(par, par2)).toBe(true);
-    expect(intersect(par2, par3)).toBe(true);    
+    expect(intersect(par2, par3)).toBe(true);
 });
 
 
--- src/utils/geometry/parallelogram.ts
+++ src/utils/geometry/parallelogram.ts
@@ -1,75 +1,75 @@
 import { Point, parallelWithinEpsilon, dot } from "./point";
- 
-export enum VertexId { Corner,  VertexA, otherCorner, VertexB }
 
-    
+export enum VertexId { Corner, VertexA, otherCorner, VertexB }
+
+
 export class Parallelogram {
-        isSeg:Boolean
-        corner: Point
-        a:Point //a side adjacent to the corner
-        b:Point //another side exiting from the corner
-        otherCorner: Point
-        aPlusCorner:Point
-        bPlusCorner:Point
-        aRot:Point //a rotated on 90 degrees towards side1
-        bRot:Point //b rotated on 90 degrees towards coeff
-        abRot:number //a*bRot 
-        baRot:number //b*aRot;
-    
-        
-        constructor() {}
-        
-        // Return true if the parallelogram contains the point
-        contains(point:Point): Boolean{
-            let g = point.minus(this.corner)
-            let e = Point.distanceEpsilon
+    isSeg: Boolean
+    corner: Point
+    a: Point //a side adjacent to the corner
+    b: Point //another side exiting from the corner
+    otherCorner: Point
+    aPlusCorner: Point
+    bPlusCorner: Point
+    aRot: Point //a rotated on 90 degrees towards side1
+    bRot: Point //b rotated on 90 degrees towards coeff
+    abRot: number //a*bRot 
+    baRot: number //b*aRot;
 
-            let gbRot = g.dot(this.bRot)
-            if (gbRot > this.abRot + e || gbRot < -e)
-                return false;
 
-            let gaRot = g.dot(this.aRot)
-            return gaRot <= this.baRot + e && gaRot >= -e;
+    constructor() { }
+
+    // Return true if the parallelogram contains the point
+    contains(point: Point): Boolean {
+        let g = point.minus(this.corner)
+        let e = Point.distanceEpsilon
+
+        let gbRot = g.dot(this.bRot)
+        if (gbRot > this.abRot + e || gbRot < -e)
+            return false;
+
+        let gaRot = g.dot(this.aRot)
+        return gaRot <= this.baRot + e && gaRot >= -e;
+    }
+
+    /// Return the correspoinding vertex of the parallelogram
+    vertex(vertexPar: VertexId): Point {
+        switch (vertexPar) {
+            case VertexId.Corner: return this.corner;
+            case VertexId.VertexA: return this.aPlusCorner;
+            case VertexId.otherCorner: return this.otherCorner;
+            case VertexId.VertexB: return this.bPlusCorner;
+            default:
+                throw undefined;
         }
+    }
 
-
-        area () { return Math.abs(this.a.x * this.b.y - this.a.y * this.b.x) } 
+    area() { return Math.abs(this.a.x * this.b.y - this.a.y * this.b.x) }
 }
 
- /// Return the correspoinding vertex of the parallelogram
- export function vertex(p:Parallelogram, vertexPar:VertexId) {
-    switch (vertexPar) {
-        case VertexId.Corner: return p.corner;
-        case VertexId.VertexA: return p.aPlusCorner;
-        case VertexId.otherCorner: return p.otherCorner;
-        case VertexId.VertexB: return p.bPlusCorner;
-        default:
-            throw undefined;
-    }
-}        
 
-function PumpMinMax(minx:number, maxx:number, miny:number, maxy:number, 
-                p:Point) {
-                if (p.x < minx) {
-                    minx = p.x;
-                } else if (p.x > maxx) {
-                    maxx = p.x;
-                }
-                if (p.y < miny) {
-                    miny = p.y;
-                } else if (p.y > maxy) {
-                    maxy = p.y;
-                }
-            }
-   
-             // returns true if parallelograms intersect
-export function intersect(parallelogram0:Parallelogram, parallelogram1:Parallelogram) {
+function PumpMinMax(minx: number, maxx: number, miny: number, maxy: number,
+    p: Point) {
+    if (p.x < minx) {
+        minx = p.x;
+    } else if (p.x > maxx) {
+        maxx = p.x;
+    }
+    if (p.y < miny) {
+        miny = p.y;
+    } else if (p.y > maxy) {
+        maxy = p.y;
+    }
+}
+
+// returns true if parallelograms intersect
+export function intersect(parallelogram0: Parallelogram, parallelogram1: Parallelogram) {
     // It can be shown that two parallelograms do not intersect if and only if
     // they are separated with one of the parallelogram sides 
 
 
     let ret = !(separByA(parallelogram0, parallelogram1) || separByA(parallelogram1, parallelogram0) ||
-               separByB(parallelogram0, parallelogram1) || separByB(parallelogram1, parallelogram0));
+        separByB(parallelogram0, parallelogram1) || separByB(parallelogram1, parallelogram0));
 
     if (ret == false)
         return false;
@@ -79,7 +79,7 @@ export function intersect(parallelogram0:Parallelogram, parallelogram1:Parallelo
 
 
     if (!parallelWithinEpsilon(parallelogram0.otherCorner.minus(parallelogram0.corner),
-                                     parallelogram1.otherCorner.minus(parallelogram1.corner), 1.0E-5))
+        parallelogram1.otherCorner.minus(parallelogram1.corner), 1.0E-5))
         return true
 
     //here we know that the segs are parallel
@@ -87,7 +87,7 @@ export function intersect(parallelogram0:Parallelogram, parallelogram1:Parallelo
 
 }
 
-function ParallelSegsIntersect(p0:Parallelogram, p1:Parallelogram) :Boolean{
+function ParallelSegsIntersect(p0: Parallelogram, p1: Parallelogram): Boolean {
     let v0 = p0.corner
     let v1 = p0.otherCorner
 
@@ -97,11 +97,11 @@ function ParallelSegsIntersect(p0:Parallelogram, p1:Parallelogram) :Boolean{
     let d = v1.minus(v0)
 
     //let us imagine that v0 is at zero
-    let r0:number = 0; // position of v0
+    let r0: number = 0; // position of v0
     let r1 = d.dot(d) //offset of v1
-    
+
     //offset of v2
-    let r2 = v2.minus(v0). dot(d)
+    let r2 = v2.minus(v0).dot(d)
 
     //offset of v3
     let r3 = v3.minus(v0).dot(d)
@@ -118,32 +118,32 @@ function ParallelSegsIntersect(p0:Parallelogram, p1:Parallelogram) :Boolean{
 
 }
 
-function separByB(p0:Parallelogram, p1:Parallelogram) {
+function separByB(p0: Parallelogram, p1: Parallelogram) {
     let eps = Point.distanceEpsilon
-    let p1a = vertex(p1, 0).minus(p0.corner).dot(p0.bRot)
-    let list = [VertexId.VertexA, VertexId.otherCorner, VertexId.VertexB]        
+    let p1a = p1.vertex(0).minus(p0.corner).dot(p0.bRot)
+    let list = [VertexId.VertexA, VertexId.otherCorner, VertexId.VertexB]
     if (p1a > p0.abRot + eps) {
         for (let i of list) {
-            if (vertex(p1, i).minus(p0.corner).dot(p0.bRot) <= p0.abRot + eps)
+            if (p1.vertex(i).minus(p0.corner).dot(p0.bRot) <= p0.abRot + eps)
                 return false;
         }
 
         return true;
     } else if (p1a < -eps) {
         for (let i of list) {
-            if (vertex(p1, i).minus(p0.corner).dot(p0.bRot) >= -eps)
+            if (p1.vertex(i).minus(p0.corner).dot(p0.bRot) >= -eps)
                 return false;
         }
         return true;
     }
     return false;
 }
-function separByA(p0:Parallelogram, p1:Parallelogram) {
+function separByA(p0: Parallelogram, p1: Parallelogram) {
 
     let eps = Point.distanceEpsilon;
 
     let t = p1.corner.minus(p0.corner)
-    let p1a = dot(t , p0.aRot);
+    let p1a = dot(t, p0.aRot);
 
     if (p1a > p0.baRot + eps) {
         t.x = p1.aPlusCorner.x - p0.corner.x;
@@ -184,133 +184,133 @@ function separByA(p0:Parallelogram, p1:Parallelogram) {
     return false
 }
 
-    /// create a Parallelogram over a group
-    function GetParallelogramOfAGroup( boxes:[Parallelogram]) {
-        let minx = 0, maxx = 0, miny = 0, maxy = 0
-        let firstTime = true;
-        for (let b of boxes) {
-            let verts = AllVertices(b)
-            for (var v of verts) {
-                let x = v.x;
-                let y = v.y;
-                if (firstTime) {
-                    firstTime = false;
-                    minx = maxx = x;
-                    miny = maxy = y;
-                } else {
-                    if (x < minx) {
-                        minx = x;
-                    } else if (x > maxx) {
-                        maxx = x;
-                    }
-                    if (y < miny) {
-                        miny = y;
-                    } else if (y > maxy) {
-                        maxy = y;
-                    }
+/// create a Parallelogram over a group
+function GetParallelogramOfAGroup(boxes: [Parallelogram]) {
+    let minx = 0, maxx = 0, miny = 0, maxy = 0
+    let firstTime = true;
+    for (let b of boxes) {
+        let verts = AllVertices(b)
+        for (var v of verts) {
+            let x = v.x;
+            let y = v.y;
+            if (firstTime) {
+                firstTime = false;
+                minx = maxx = x;
+                miny = maxy = y;
+            } else {
+                if (x < minx) {
+                    minx = x;
+                } else if (x > maxx) {
+                    maxx = x;
+                }
+                if (y < miny) {
+                    miny = y;
+                } else if (y > maxy) {
+                    maxy = y;
                 }
             }
         }
-        return parallelogramByCornerSideSide(new Point(minx, miny), 
-        new Point(0, maxy - miny), 
-        new Point(maxx - minx, 0));
     }
+    return parallelogramByCornerSideSide(new Point(minx, miny),
+        new Point(0, maxy - miny),
+        new Point(maxx - minx, 0));
+}
 
 
-    export function parallelogramByCornerSideSide(corner:Point, sideA:Point, sideB:Point): Parallelogram {
-        const result = new Parallelogram();
+export function parallelogramByCornerSideSide(corner: Point, sideA: Point, sideB: Point): Parallelogram {
+    const result = new Parallelogram();
 
-        result.corner = corner;
-        result.a = sideA;
-        result.b = sideB;
+    result.corner = corner;
+    result.a = sideA;
+    result.b = sideB;
 
-        result.aRot = new Point(-sideA.y, sideA.x);
-        if (result.aRot.length() > 0.5)
+    result.aRot = new Point(-sideA.y, sideA.x);
+    if (result.aRot.length() > 0.5)
         result.aRot = result.aRot.normalize();
 
-        result.bRot = new Point(-sideB.y, sideB.x);
-        if (result.bRot.length() > 0.5)
+    result.bRot = new Point(-sideB.y, sideB.x);
+    if (result.bRot.length() > 0.5)
         result.bRot = result.bRot.normalize();
 
-        result.abRot = result.bRot.dot(sideA)
+    result.abRot = result.bRot.dot(sideA)
 
-        result.baRot = sideB.dot(result.aRot)
+    result.baRot = sideB.dot(result.aRot)
 
-        if (result.abRot < 0) {
-            result.abRot = -result.abRot;
-            result.bRot = result.bRot.neg();
-        }
-
-        if (result.baRot < 0) {
-            result.baRot = -result.baRot
-            result.aRot = result.aRot.neg()
-        }
-
-        result.isSeg = sideA.minus(sideB).length() < Point.distanceEpsilon;
-
-        result.aPlusCorner = sideA.add(corner);
-        result.otherCorner =  sideB.add(result.aPlusCorner);
-        result.bPlusCorner = sideB.add(corner);
-        
-        return result;
+    if (result.abRot < 0) {
+        result.abRot = -result.abRot;
+        result.bRot = result.bRot.neg();
     }
 
-   export function parallelogramOfTwo(box0:Parallelogram, box1:Parallelogram): Parallelogram {
-        let result = new Parallelogram();
-        let v = box0.corner
-        let minx = v.x, maxx = v.x, miny = v.y, maxy = v.y
-        
-
-        PumpMinMax(minx,  maxx,  miny,  maxy,  box0.aPlusCorner);
-        PumpMinMax( minx,  maxx,  miny,  maxy,  box0.otherCorner);
-        PumpMinMax( minx,  maxx,  miny,  maxy,  box0.bPlusCorner);
-
-        PumpMinMax( minx,  maxx,  miny,  maxy,  box1.corner);
-        PumpMinMax( minx,  maxx,  miny,  maxy,  box1.aPlusCorner);
-        PumpMinMax( minx,  maxx,  miny,  maxy,  box1.otherCorner);
-        PumpMinMax( minx,  maxx,  miny,  maxy,  box1.bPlusCorner);
-
-        result.corner = new Point(minx, miny);
-        result.a = new Point(0, maxy - miny);
-        result.b = new Point(maxx - minx, 0);
-
-        result.aPlusCorner = result.a.add(result.corner);
-        result.otherCorner = result.b.add(result.aPlusCorner);
-        result.bPlusCorner = result.b.add(result.corner);
-
-        result.aRot = new Point(-result.a.y, result.a.x);
-        if (result.aRot.length() > 0.5)
-            result.aRot = result.aRot.normalize();
-
-        result.bRot = new Point(-result.b.y, result.b.x);
-        if (result.bRot.length() > 0.5)
-        result.bRot = result.bRot.normalize()   
-
-        result.abRot = result.a.dot( result.bRot)
-        result.baRot = result.b.dot(result.aRot)
-
-
-        if (result.abRot < 0) {
-            result.abRot = -result.abRot;
-            result.bRot = result.bRot.neg();
-        }
-
-        if (result.baRot < 0) {
-            result.baRot = -result.baRot;
-            result.aRot = result.aRot.neg();
-        }
-
-        result.isSeg = result.a.minus(result.b).length() < Point.distanceEpsilon
-        return result
+    if (result.baRot < 0) {
+        result.baRot = -result.baRot
+        result.aRot = result.aRot.neg()
     }
- 
- function *AllVertices(p:Parallelogram)  {            
-        yield p.corner
-        yield p.aPlusCorner
-        yield p.otherCorner
-        yield p.bPlusCorner
+
+    result.isSeg = sideA.minus(sideB).length() < Point.distanceEpsilon;
+
+    result.aPlusCorner = sideA.add(corner);
+    result.otherCorner = sideB.add(result.aPlusCorner);
+    result.bPlusCorner = sideB.add(corner);
+
+    return result;
 }
-function *OtherVertices (p:Parallelogram) {
+
+export function parallelogramOfTwo(box0: Parallelogram, box1: Parallelogram): Parallelogram {
+    let result = new Parallelogram();
+    let v = box0.corner
+    let minx = v.x, maxx = v.x, miny = v.y, maxy = v.y
+
+
+    PumpMinMax(minx, maxx, miny, maxy, box0.aPlusCorner);
+    PumpMinMax(minx, maxx, miny, maxy, box0.otherCorner);
+    PumpMinMax(minx, maxx, miny, maxy, box0.bPlusCorner);
+
+    PumpMinMax(minx, maxx, miny, maxy, box1.corner);
+    PumpMinMax(minx, maxx, miny, maxy, box1.aPlusCorner);
+    PumpMinMax(minx, maxx, miny, maxy, box1.otherCorner);
+    PumpMinMax(minx, maxx, miny, maxy, box1.bPlusCorner);
+
+    result.corner = new Point(minx, miny);
+    result.a = new Point(0, maxy - miny);
+    result.b = new Point(maxx - minx, 0);
+
+    result.aPlusCorner = result.a.add(result.corner);
+    result.otherCorner = result.b.add(result.aPlusCorner);
+    result.bPlusCorner = result.b.add(result.corner);
+
+    result.aRot = new Point(-result.a.y, result.a.x);
+    if (result.aRot.length() > 0.5)
+        result.aRot = result.aRot.normalize();
+
+    result.bRot = new Point(-result.b.y, result.b.x);
+    if (result.bRot.length() > 0.5)
+        result.bRot = result.bRot.normalize()
+
+    result.abRot = result.a.dot(result.bRot)
+    result.baRot = result.b.dot(result.aRot)
+
+
+    if (result.abRot < 0) {
+        result.abRot = -result.abRot;
+        result.bRot = result.bRot.neg();
+    }
+
+    if (result.baRot < 0) {
+        result.baRot = -result.baRot;
+        result.aRot = result.aRot.neg();
+    }
+
+    result.isSeg = result.a.minus(result.b).length() < Point.distanceEpsilon
+    return result
+}
+
+function* AllVertices(p: Parallelogram) {
+    yield p.corner
+    yield p.aPlusCorner
+    yield p.otherCorner
+    yield p.bPlusCorner
+}
+function* OtherVertices(p: Parallelogram) {
     yield p.aPlusCorner
     yield p.otherCorner
     yield p.bPlusCorner
